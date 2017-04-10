72e40a950e3cb44d8f6543576366fd04d83444cf
AlexandrTheMacedonian <Sanyasemenov96@gmail.com>
2017-04-10 22:37:40 +0300

    3

--- src/main/java/ua/alex/SpringApp/controller/UserController.java
+++ src/main/java/ua/alex/SpringApp/controller/UserController.java
@@ -1,6 +1,5 @@
 package ua.alex.SpringApp.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
